0619399cee8f8fe944018a09a06e0add6e2c41c8
Alberto Chacon <luis1122ch@gmail.com>
2022-11-03 20:56:22 -0400

    Creacion de base de datos de evaluaciones

--- SARPtesis/Login/admin.py
+++ SARPtesis/Login/admin.py
@@ -1,7 +1,9 @@
 from tkinter import S
 from django.contrib import admin
-from Login.models import Staff, Department
+from Login.models import Staff, Department, Questions, Evaluations
 
 # Register your models here.
 admin.site.register(Staff)
 admin.site.register(Department)
+admin.site.register(Evaluations)
+admin.site.register(Questions)
--- SARPtesis/Login/models.py
+++ SARPtesis/Login/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+# Models Questions
+class Questions(models.Model):
+    question = models.CharField(max_length=300)
+    value = models.IntegerField(max_length=300)
+
+# Models Evaluations
+class Evaluations(models.Model):
+    title = models.CharField(max_length=300)
+    question = models.ForeignKey(Questions, on_delete=models.SET_NULL, null=True)
+
 # Models department
 class Department(models.Model):
     management = models.CharField(max_length=300)
@@ -15,7 +25,7 @@ class  Staff(models.Model):
     name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
-    number = models.CharField(max_length=50)
+    number = models.IntegerField(max_length=50)
     jobs = models.CharField(max_length=300)
     management = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
 
